b4b8b2bd860fa901f7e1fa2bb5323972861d6e6e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-17 13:58:06 +0700

    Membuat user component dengan livewire

---
+++ resources/views/livewire/users-component.blade.php
@@ -0,0 +1,39 @@
+<!-- Table Start -->
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-12">
+            <div class="bg-light rounded h-100 p-4">
+                <div class="d-flex justify-content-between align-items-center mb-4">
+                    <h6 class="m-0">Basic Table</h6>
+                    <button wire:click="create" class="btn btn-primary btn-sm">+ Tambah</button>
+                </div>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No.</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Role</th>
+                            <th>
+
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($user as $data)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}.</th>
+                                <td>{{ $data->name }}</td>
+                                <td>{{ $data->email }}</td>
+                                <td>{{ $data->role }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Table End -->
