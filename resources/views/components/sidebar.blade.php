5bc435c23ea41263d2bf52bfe4e998739a0d4974
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-17 13:59:19 +0700

    Menambah route user pada sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -21,7 +21,7 @@
             <a href="widget.html" class="nav-item nav-link"><i class="fa fa-shopping-cart me-2"></i>Transaksi</a>
             <a href="form.html" class="nav-item nav-link"><i class="fa fa-file-archive me-2"></i>Laporan</a>
             <a href="table.html" class="nav-item nav-link"><i class="fa fa-car me-2"></i>Mobil</a>
-            <a href="chart.html" class="nav-item nav-link"><i class="fa fa-users me-2"></i>User</a>
+            <a href="{{ route('users') }}" class="nav-item nav-link"><i class="fa fa-users me-2"></i>User</a>
         </div>
     </nav>
 </div>
