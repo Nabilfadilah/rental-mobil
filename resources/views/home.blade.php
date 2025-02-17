437edc5e26ca1451457696ad1676f874c13e62bd
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-17 13:56:52 +0700

    Menambah include component card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', 'Home - Rental Mobil')
 
+
 @section('content')
+    @include('components.card')
+    <div class="container-fluid pt-4 px-4">
+        <div class="row g-4">
+        </div>
+    </div>
 
 @endsection
