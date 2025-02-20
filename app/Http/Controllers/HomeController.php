a04954aa9517746a46c4d5d87ae575e50cf2e26f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-20 17:35:38 +0700

    Menambah controller home index data transaksi, mobil dan user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mobil;
+use App\Models\Transaksi;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $data['transaksi'] = Transaksi::where('status', 'SELESAI')->sum('total');
+        $data['mobil'] = Mobil::count();
+        $data['user'] = User::count();
+        return view('home', $data);
     }
 }
